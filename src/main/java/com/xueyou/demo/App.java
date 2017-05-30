44c909d36fd3a1d5e71946d4c2b8677c08294e0c
WuXueyou <409898859@qq.com>
2017-05-31 07:59:11 +0800

    commit

--- src/main/java/com/xueyou/demo/App.java
+++ src/main/java/com/xueyou/demo/App.java
@@ -5,15 +5,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+
 /**
  * Hello world!
  */
 @SpringBootApplication
-@ComponentScan(basePackages = {"controller","com.xueyou.demo","schedule"})
+@ComponentScan(basePackages = {"controller", "com.xueyou.demo", "schedule"})
 @EnableScheduling
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        SpringApplication.run(App.class,args);
+        SpringApplication.run(App.class, args);
     }
 }
