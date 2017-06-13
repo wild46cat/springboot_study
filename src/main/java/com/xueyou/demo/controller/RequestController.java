7ab5a44dfe83bd5e161e8bc20025d336d94d19a7
WuXueyou <409898859@qq.com>
2017-06-14 00:57:38 +0800

    add CrossOrigin and the angularjs GET POST API.

--- src/main/java/com/xueyou/demo/controller/RequestController.java
+++ src/main/java/com/xueyou/demo/controller/RequestController.java
@@ -1,6 +1,7 @@
 package com.xueyou.demo.controller;
 
 import com.xueyou.demo.pojo.Student;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,12 +13,22 @@ import java.util.List;
  * Created by wuxueyou on 2017/6/13.
  */
 @RestController
+@CrossOrigin
 @RequestMapping("/request")
 public class RequestController {
 
-    @RequestMapping(value = "/get",method = {RequestMethod.GET})
-    public List<Student> getStudentList(){
+    @RequestMapping(value = "/get", method = {RequestMethod.GET})
+    public List<Student> getStudentList() {
         Student student = new Student(1, "xiaoming");
+        List<Student> studentList = new ArrayList<>();
+        studentList.add(student);
+        return studentList;
+    }
+
+
+    @RequestMapping(value = "/post", method = {RequestMethod.POST})
+    public List<Student> postStudentList() {
+        Student student = new Student(2, "xiaohong");
         List<Student> studentList = new ArrayList<>();
         studentList.add(student);
         return studentList;
