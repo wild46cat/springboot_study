e949c7cc57061c31804743b67912a1952224bed1
WuXueyou <409898859@qq.com>
2017-12-24 12:43:08 +0800

    添加leader选举共鞥

---
+++ src/main/java/com/xueyou/demo/leader/SelectLeader.java
@@ -0,0 +1,54 @@
+package com.xueyou.demo.leader;
+
+import com.xueyou.demo.utils.zkUtils.CreateClient;
+import com.xueyou.demo.utils.zkUtils.CuratorZkClientBridge;
+import org.apache.curator.framework.CuratorFramework;
+import org.apache.curator.framework.recipes.leader.LeaderSelector;
+import org.apache.curator.framework.recipes.leader.LeaderSelectorListener;
+import org.apache.curator.framework.recipes.leader.LeaderSelectorListenerAdapter;
+import org.springframework.stereotype.Component;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * Created by wuxueyou on 2017/12/11.
+ */
+@Component
+public class SelectLeader {
+    private static String connectionString = "localhost:2181";
+    private static String ID = "2";
+
+    public SelectLeader() {
+        try {
+            init();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void init() throws Exception {
+        CuratorFramework curatorFramework = CreateClient.createSimple(connectionString);
+        curatorFramework.start();
+        //doSomething to zookeeper
+        CuratorZkClientBridge curatorZkClientBridge = new CuratorZkClientBridge(curatorFramework);
+
+//        System.out.println(getNode(curatorZkClientBridge, "/"));
+        //master 选举
+        LeaderSelectorListener listener = new LeaderSelectorListenerAdapter() {
+            public void takeLeadership(CuratorFramework client) throws Exception {
+                // this callback will get called when you are the leader
+                // do whatever leader work you need to and only exit
+                // this method when you want to relinquish leadership
+                System.out.println("i am master" + "\t" + ID + "\t" + new SimpleDateFormat("HH:mm:ss").format(new Date()));
+                Thread.sleep(Integer.MAX_VALUE);
+            }
+        };
+
+        LeaderSelector selector = new LeaderSelector(curatorFramework, "/xytestleader/master", listener);
+        selector.autoRequeue();  // not required, but this is behavior that you will probably expect
+        selector.start();
+    }
+
+
+}
--- src/main/java/com/xueyou/demo/schedule/ScheduleTask.java
+++ src/main/java/com/xueyou/demo/schedule/ScheduleTask.java
@@ -2,15 +2,18 @@ package com.xueyou.demo.schedule;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import java.text.SimpleDateFormat;
+import java.util.Date;
 
 @Component
 public class ScheduleTask {
     private static final Logger LOGGER = LoggerFactory.getLogger(ScheduleTask.class);
     private static final SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
     private static final long INTERVALTIME = 3000L;
+
 
 //    @Scheduled(fixedRate = INTERVALTIME)
 //    public void reportCurrentTime() {
